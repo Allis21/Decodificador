2486bd9a5c30d25dbea3e1b0c8547470376b8d01
Allis21 <allisonhincapie21@gmail.com>
2024-08-17 18:57:59 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Decodificador-Alura
+
+## Descripción
+El proyecto "Decodificador-Alura" es una aplicación web que permite encriptar y desencriptar mensajes de texto. La aplicación asegura que el texto ingresado no contenga letras mayúsculas, números o caracteres especiales antes de proceder con la encriptación o desencriptación.
+
+## Características
+- Encriptación de mensajes de texto.
+- Desencriptación de mensajes de texto.
+- Validación de texto para evitar letras mayúsculas, números y caracteres especiales.
+- Interfaz responsive para tablets y celulares.
+
+## Tecnologías Utilizadas
+- HTML
+- CSS
+- JavaScript
+
+## Instalación
+1. Clona el repositorio:
+   ```bash
+   git clone https://github.com/tu-usuario/Decodificador-Alura.git
---
+++ app.js
@@ -0,0 +1,84 @@
+const textArea = document.querySelector(".textarea");
+const mensaje = document.querySelector(".mensaje");
+const tituloResultado = document.querySelector(".tituloResultado")
+const comentarioResultado = document.querySelector(".comentarioResultado")
+const buttonCopiar = document.querySelector(".buttonCopiar")
+
+function contieneMayusculasOcaracteresEspeciales(textArea){
+    return /[A-Z]/.test(textArea) || /[^a-z ]/.test(textArea) || /[" "]/.test(textArea)
+}
+
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+
+function buttonEncriptar(){
+    if (contieneMayusculasOcaracteresEspeciales(textArea.value)) {
+        alert("El texto no debe contener mayúsculas o caracteres especiales");
+        return limpiarCaja(); 
+    }
+    
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none"
+    tituloResultado.style.display = "none"
+    comentarioResultado.style.display = "none"
+    buttonCopiar.style.display = "block"
+    
+}
+
+function encriptar(stringEncriptada){
+    stringEncriptada = removeSpecialCharacters(stringEncriptada);
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+    return stringEncriptada
+}
+
+function buttonDesencriptar(){
+    if (contieneMayusculasOcaracteresEspeciales(textArea.value)) {
+        alert("El texto no debe contener mayúsculas o caracteres especiales");
+        return limpiarCaja(); 
+    }
+    
+    const textoDesencriptado = desencriptar(textArea.value);
+    mensaje.value = textoDesencriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none"
+    tituloResultado.style.display = "none"
+    comentarioResultado.style.display = "none"
+    buttonCopiar.style.display = "block"
+    
+}
+
+function desencriptar(stringDesencriptada){
+    stringDesencriptada = removeSpecialCharacters(stringDesencriptada);
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase()
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        }
+    }
+    return stringDesencriptada
+}
+
+function copyText(){
+    navigator.clipboard.writeText(mensaje.value)
+    alert("Texto copiado al portapapeles")
+}
+
+function limpiarCaja() {
+    document.querySelector(".textarea").value = '';
+}
+
+
